a540408ea2fc286c7c230569ff9c6a1588d63f34
jtenca <developertenca@outlook.com>
2020-03-23 17:05:25 -0300

    iniciando projeto

---
+++ ContaBanco.php
@@ -0,0 +1,28 @@
+<?php
+class ContaBanco {
+    public $numConta;
+    protected $tipoConta;
+    private $donoConta;
+    private $saldoConta;
+    private $statusConta;
+
+    public function abrirConta() {
+
+    }
+
+    public function fecharConta() {
+
+    }
+
+    public function depositar() {
+
+    }
+
+    public function sacar() {
+
+    }
+
+    public function pagarMensal() {
+        
+    }
+}
